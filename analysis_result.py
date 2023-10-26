6e10565eea1e62c4e695477edb5a92bc5d6940fa
Duong Tran <d@dttt.io>
2023-10-27 00:59:59 +0200

    * Analysis result

---
+++ analysis_result.py
@@ -0,0 +1,168 @@
+import ast
+import csv
+from pathlib import Path
+from sentence_transformers import SentenceTransformer, util
+import torch
+
+METRICS = ['exact_match', 'substring', 'similarity']
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+similarity_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', device=device)
+
+
+class Score:
+    exact_match: int = 0
+    substring: int = 0
+    similarity: float = 0.0
+    vqa_acc: float = 0.0
+
+    def __getitem__(self, item):
+        if item == 'exact_match':
+            return self.exact_match
+        if item == 'substring':
+            return self.substring
+        if item == 'similarity':
+            return self.similarity
+        if item == 'vqa_acc':
+            return self.vqa_acc
+
+    def __setitem__(self, key, value):
+        if key == 'exact_match':
+            self.exact_match = value
+        if key == 'substring':
+            self.substring = value
+        if key == 'similarity':
+            self.similarity = value
+        if key == 'vqa_acc':
+            self.vqa_acc = value
+
+    def __str__(self):
+        return f'exact_match: {self.exact_match}; substring: {self.substring}; similarity: {self.similarity:.2f}; vqa_acc: {self.vqa_acc:.2f}; '
+
+
+# gt is the ground truth list of answers
+def exact_match_score(pred, gt):
+    return 1 if pred in gt else 0
+
+
+def substring_score(pred, gt):
+    for s in gt:
+        if pred in s:
+            return 1
+    return 0
+
+
+# https://huggingface.co/tasks/sentence-similarity
+def similarity_score(pred, gt):
+    max_score = 0
+    pred_emb = similarity_model.encode(pred, convert_to_tensor=True, device=device)
+    for s in gt:
+        emb = similarity_model.encode(s, convert_to_tensor=True, device=device)
+        current_score = util.pytorch_cos_sim(pred_emb, emb)
+        if current_score > max_score:
+            max_score = current_score
+    return max_score
+
+
+# https://visualqa.org/evaluation.html
+def vqa_acc(pred, gt):
+    return 0.0
+
+
+# depending on the model, answer might be given within a complete sentence. e.g.: [answer] The length is 300 meters
+# we need to extract "The length is 300 meters" only
+def extract_answer(answer_text):
+    return answer_text
+
+
+def get_ratio(a, b):
+    if b == 0:
+        return 0
+    return a / b
+
+
+def analysis_result(list_of_result_dir):
+    total = 0
+    score = Score()
+
+    total_by_hop = {}
+    score_by_hop = {}
+
+    total_by_scene_graph = {
+        'with': 0,
+        'without': 0
+    }
+    score_by_scene_graph = {
+        'with': Score(),
+        'without': Score()
+    }
+
+    total_by_ds = {
+        'VG': 0,
+        'GLDv2': 0
+    }
+    score_by_ds = {
+        'VG': Score(),
+        'GLDv2': Score()
+    }
+
+    for folder in list_of_result_dir:
+        for csvfile in Path(folder).iterdir():
+            csv_file = f'{csvfile.parent}/{csvfile.name}'
+            with open(csv_file) as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    # there's a bug that the answer set is empty, ignore them
+                    answer_str = row['answer'].lower()
+                    answer = ast.literal_eval(answer_str)
+                    if len(answer) == 0:
+                        continue
+
+                    total += 1
+
+                    n_hop = row['n_hop']
+                    if n_hop not in total_by_hop:
+                        total_by_hop[n_hop] = 0
+                        score_by_hop[n_hop] = Score()
+                    total_by_hop[n_hop] += 1
+
+                    if row['has_scene_graph']:
+                        total_by_scene_graph['with'] += 1
+                    else:
+                        total_by_scene_graph['without'] += 1
+
+                    ds_name = 'VG' if row['id'].startswith('VG_') else 'GLDv2'
+                    total_by_ds[ds_name] += 1
+
+                    prediction_str = row['prediction'].lower()
+                    prediction = extract_answer(ast.literal_eval(prediction_str)[0])
+
+                    # compute all scores
+                    current_score = Score()
+                    if 'exact_match' in METRICS:
+                        current_score.exact_match = exact_match_score(prediction, answer)
+                    if 'substring' in METRICS:
+                        current_score.substring = substring_score(prediction, answer)
+                    if 'similarity' in METRICS:
+                        current_score.similarity = similarity_score(prediction, answer)
+
+                    for s in METRICS:
+                        score[s] += current_score[s]
+                        score_by_hop[n_hop][s] += current_score[s]
+                        score_by_scene_graph['with' if row['has_scene_graph'] else 'without'][s] += current_score[s]
+                        score_by_ds[ds_name][s] += current_score[s]
+
+    for s in METRICS:
+        print('=====', s)
+        print('Total:', total, ', Correct:', score, 'Acc:', f'{get_ratio(score[s], total):.2f}')
+        for h in score_by_hop.keys():
+            print(f'{h}-hop:', f'{get_ratio(score_by_hop[h][s], total_by_hop[n_hop]):.2f}')
+        print('W/ Scene graph:', f"{get_ratio(score_by_scene_graph['with'][s], total_by_scene_graph['with']):.2f}")
+        print('W/O Scene graph:',
+              f"{get_ratio(score_by_scene_graph['without'][s], total_by_scene_graph['without']):.2f}")
+        for ds in total_by_ds.keys():
+            print(ds, f"{get_ratio(score_by_ds[ds][s], total_by_ds[ds]):.2f}")
+
+
+if __name__ == '__main__':
+    analysis_result(['result/output_balanced_10', 'result/output_balanced_10_test'])
