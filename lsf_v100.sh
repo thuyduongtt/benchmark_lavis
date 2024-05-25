268b3324e4963cbf5618d30eb5244eda24422c04
Duong Tran <d@dttt.io>
2024-05-25 22:52:02 +0200

    Added more datasets

--- lsf_v100.sh
+++ lsf_v100.sh
@@ -1,4 +1,4 @@
-#BSUB -J BLIP[11]                 # Specify the job name
+#BSUB -J BLIP[1-2]                 # Specify the job name
 #BSUB -W 168:00                # Specify the maximum runtime in "hours:minutes"
 #BSUB -o %x.%j.out            # Determine where the output will be written
 #BSUB -e %x.%j.err            # The same goes for the error file
@@ -35,7 +35,7 @@ module load conda
  
 # Perform experiments
 source activate lavis
-./run.sh $LSB_JOBINDEX
+./scripts/run_OKVQA.sh $LSB_JOBINDEX
 
 
 # No longer exit on any error.
